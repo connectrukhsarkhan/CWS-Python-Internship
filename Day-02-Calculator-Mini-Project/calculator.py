0f81af720632a14a6d5943df5c7a6deccb10c819
connectrukhsarkhan <connect.rukhsarkhan@gmail.com>
2026-01-16 21:45:00 +0530

    Update calculator.py

--- Day-02-Calculator-Mini-Project/calculator.py
+++ Day-02-Calculator-Mini-Project/calculator.py
@@ -1,1 +1,41 @@
-Day 02 calculator mini project added
+print("Welcome to calculator application...")
+first = float(input("Enter the first number: "))
+
+
+print("Select any one opeartor: ")
+ope = input(" Enter the operator/symbol: ")
+
+'''
+#using match case
+match(ope):
+    case "+":
+        second= float(input("Enter the second number: "))
+        print(first+second)
+    case "-":
+         second= float(input("Enter the second number: "))
+         print(first-second)
+    case "*":
+          second= float(input("Enter the second number: "))
+          print(first * second)
+    case "/":
+           second= float(input("Enter the second number: "))
+           print(first/second)
+           
+ '''                            
+#using if elif 
+if(ope== "+"):
+           second= float(input("Enter the second number: "))
+           print(first + second)
+          
+elif(ope== "-"):
+           second= float(input("Enter the second number: "))
+           print(first - second)        
+            
+elif(ope== "*"):
+               second= float(input("Enter the second number: "))
+               print(first * second)
+          
+elif(ope == "/"):
+               second= float(input("Enter the second number: "))
+               print(first / second)          
+               Day 02 calculator mini project added
