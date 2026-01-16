f0612ff7ba4f4ecea1196696c416c325b61ff907
connectrukhsarkhan <connect.rukhsarkhan@gmail.com>
2026-01-16 09:22:40 +0530

    Day 01 basic python program added

---
+++ Day-01-Python-Introduction/myFitstProgram.py
@@ -0,0 +1,14 @@
+# Day 01 - Python Introduction
+# Topic: print() and input() function
+
+print("Welcome to Python Programming")
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+age = input("Enter your age: ")
+print("Your age is", age)
+
+address =input("enter your address ")
+print("your addresss:",address)
+print("Thank you for using this program")
